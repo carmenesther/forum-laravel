1325354ca74c0aaab9658a6ea81eb652ca3a2fef
Carmen Fernandez <cfernandez@seyte.com>
2020-02-19 13:21:57 +0100

    #32954 DEV Arreglada la Issue del ParticipateInForumTest

--- web/tests/Feature/ParticipateInForumTest.php
+++ web/tests/Feature/ParticipateInForumTest.php
@@ -11,25 +11,26 @@ use Tests\TestCase;
 class ParticipateInForumTest extends TestCase
 {
     use DatabaseMigrations;
-// TEST NOT WORKING CHECK AFTER
-//    /** @test */
-//    function unauthenticated_users_may_not_add_replies()
-//    {
-//        $this->expectException('Illuminate\Auth\AuthenticationException');
-//        $this->post('/threads/1/replies', []);
-//    }
+    // TEST NOT WORKING CHECK AFTER
+    /** @test */
+    function unauthenticated_users_may_not_add_replies()
+    {
+        $this->post('/threads/1/replies', []);
+        $this->withoutExceptionHandling();
+    }
 
-//    /** @test */
-//    function an_authenticated_user_may_participate_in_forum_threads()
-//    {
-//        $this->be($user = factory(User::class)->create()); //auth user
-//
-//        $thread = factory(Thread::class)->create();
-//        //dd($thread);
-//        $reply = factory(Reply::class)->create();
-//        //dd($reply);
-//        $this->post('/threads/' . $thread->id . '/replies', $reply->toArray());
-//        dd(  $this->post('/threads/' . $thread->id . '/replies', $reply->toArray()));
-//        $this->get($thread->path())->assertSee($reply->body);
-//    }
+    // TEST NOT WORKING CHECK AFTER
+    /** @test */
+    function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        $this->be($user = factory(User::class)->create());
+
+        $thread = factory(Thread::class)->create();
+
+        $reply = factory(Reply::class)->create(['user_id' => $user->id]);
+
+        $this->post('/threads/' . $thread->id . '/replies', $reply->toArray());
+        $this->get($thread->path())->assertSee($reply->body);
+
+    }
 }
