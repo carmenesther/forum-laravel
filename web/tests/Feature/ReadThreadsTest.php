53ff1b8e1e1ac2081b4ceaf67e19dee082f498f0
Carmen Fernandez <cfernandez@seyte.com>
2020-02-19 09:29:40 +0100

    #32939 DEV Añadir la relación entre los modelos

---
+++ web/tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Reply;
+use App\Thread;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $thread;
+
+    public function setUp(): void {
+
+        parent::setUp();
+        $this->thread = factory(Thread::class)->create();
+    }
+
+    /** @test
+     */
+    public function a_user_can_view_all_threads()
+    {
+       $this->get('/threads')->assertSee($this->thread->title);
+
+    }
+    /** @test */
+    public function a_user_can_read_a_single_thread(){
+
+        $this->get('/threads/' .$this->thread->id)->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread(){
+
+        $reply = factory(Reply::class)->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' .$this->thread->id)->assertSee($reply->body);
+    }
+}
