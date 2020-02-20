fb00a5d44210914eda44e8dd2fd3b87c98b9baf7
Carmen Fernandez <cfernandez@seyte.com>
2020-02-20 09:39:38 +0100

    #32969 Creación de los helpers para el testing

---
+++ web/tests/Feature/ModelTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class $CLASS$ extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function it_$METHODS$()
+    {
+        $END$
+    }
+}
