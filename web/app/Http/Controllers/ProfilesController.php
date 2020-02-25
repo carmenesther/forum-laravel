2bcf82b5238dd2413885b28464a0e1d478f31a77
Carmen Fernandez <cfernandez@seyte.com>
2020-02-25 12:07:30 +0100

    #33171 DEV Edicion del template de profile

--- web/app/Http/Controllers/ProfilesController.php
+++ web/app/Http/Controllers/ProfilesController.php
@@ -7,10 +7,13 @@ use App\User;
 class ProfilesController extends Controller
 {
     public function show(User $user){
+        $activities = $user->activity()->latest()->with('subject')->get()->groupBy(function ($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
         return view('profiles.show',
         [
             'profileUser' => $user,
-            'threads' => $user->threads()->paginate(30)
+            'activities' => $activities
         ]);
     }
 }
