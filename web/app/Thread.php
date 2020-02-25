61c1369aa73b10fff265bbdfa49876e4e61b6678
Carmen Fernandez <cfernandez@seyte.com>
2020-02-25 10:06:04 +0100

    #33164 DEV Crear actividad del feed

--- web/app/Thread.php
+++ web/app/Thread.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Thread extends Model
 {
+    use RecordsActivity;
 
     protected $guarded = [];
     protected $with = ['creator', 'channel'];
@@ -24,6 +25,7 @@ class Thread extends Model
         static::deleting(function ($thread) {
             $thread->replies()->delete();
         });
+
     }
 
     public function path(){
