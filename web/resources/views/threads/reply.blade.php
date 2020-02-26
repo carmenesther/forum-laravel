1eb4891384f5ad5b2cfeb81a7c42e0edcad61e1c
Carmen Fernandez <cfernandez@seyte.com>
2020-02-26 10:54:38 +0100

    #33215 DEV Adicion de la funcionalidad editar reply con vue

--- web/resources/views/threads/reply.blade.php
+++ web/resources/views/threads/reply.blade.php
@@ -1,33 +1,44 @@
-<div id="reply-{{$reply->id}}" class="card">
-    <div class="card-header">
-        <div class="form-inline">
-            <h5 class="flex ">
-                <a href="{{route('profile', $reply->owner)}}">
-                    {{ $reply->owner->name }}
-                </a> said {{ $reply->created_at->diffForHumans() }}...
-            </h5>
-            <div>
-                <form action="/replies/{{ $reply->id }}/favorites" method="POST">
-                    @csrf
-                    <button type="submit" class="btn btn-outline-secondary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                        {{ $reply->favorites_count}} {{ Str::plural('Favorite', $reply->favorites_count) }}
-                    </button>
-                </form>
+<reply :attributes="{{ $reply }}" inline-template v-cloak>
+    <div id="reply-{{$reply->id}}" class="card">
+        <div class="card-header">
+            <div class="form-inline">
+                <h5 class="flex ">
+                    <a href="{{route('profile', $reply->owner)}}">
+                        {{ $reply->owner->name }}
+                    </a> said {{ $reply->created_at->diffForHumans() }}...
+                </h5>
+                <div>
+                    <form action="/replies/{{ $reply->id }}/favorites" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-outline-dark" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                            {{ $reply->favorites_count}} {{ Str::plural('Favorite', $reply->favorites_count) }}
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="card-body">
-        {{$reply->body}}
-    </div>
+        <div class="card-body">
+            <div v-if="editing">
+                <div class="form-group">
+                    <textarea class="form-control" v-model="body"></textarea>
+                </div>
+                <button class="btn btn-xs btn-primary" @click="update">Update</button>
+                <button class="btn btn-xs btn-link" @click="editing = false">Cancel</button>
+            </div>
 
-    @can('update', $reply)
-        <div class="card-footer">
-            <form action="/replies/{{$reply->id}}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-            </form>
+            <div v-else v-text="body"></div>
         </div>
-    @endcan
 
-</div>
+        @can('update', $reply)
+            <div class="card-footer level">
+                <button type="submit" class="btn btn-dark btn-sm mr-1" @click="editing = true">Edit</button>
+                <form action="/replies/{{$reply->id}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                </form>
+            </div>
+        @endcan
+
+    </div>
+</reply>
