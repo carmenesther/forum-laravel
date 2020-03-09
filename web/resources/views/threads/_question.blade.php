8cb7b78b22240a64b97bd746ae1d0419079ad6e6
Carmen Fernandez <cfernandez@seyte.com>
2020-03-09 13:44:29 +0100

    #33425 DEV Incorporada la funcionalidad de actualizar threads con AJAX

--- web/resources/views/threads/_question.blade.php
+++ web/resources/views/threads/_question.blade.php
@@ -2,21 +2,21 @@
 <div class="card" v-if="editing">
     <div class="card-header">
         <div class="level">
-            <input type="text" value="{{$thread->title}}" class="form-control">
+            <input type="text" class="form-control" v-model="form.title">
         </div>
     </div>
 
     <div class="card-body">
         <div class="form-group">
-            <textarea class="form-control" rows="10">{{$thread->body}}</textarea>
+            <textarea class="form-control" rows="10" v-model="form.body"></textarea>
         </div>
     </div>
 
     <div class="card-footer">
         <div class="level">
             <button class="btn btn-secondary btn-sm level-item" @click="editing = true" v-show="! editing">Edit</button>
-            <button class="btn btn-primary btn-sm level-item" @click="">Update</button>
-            <button class="btn btn-secondary btn-sm level-item" @click="editing = false">Cancel</button>
+            <button class="btn btn-primary btn-sm level-item" @click="update">Update</button>
+            <button class="btn btn-secondary btn-sm level-item" @click="resetForm">Cancel</button>
             @can('update', $thread)
                 <form action="{{ $thread->path() }}" method="POST" class="ml-a">
                     @csrf
@@ -36,16 +36,14 @@
             <span class="flex">
                 <a href="{{ route('profile', $thread->creator) }}">
                 {{$thread->creator->name}} </a> posted:
-                 {{$thread->title}}
+                 <span v-text="title"></span>
             </span>
         </div>
     </div>
 
-    <div class="card-body">
-        {{$thread->body}}
-    </div>
+    <div class="card-body" v-text="body"></div>
 
-    <div class="card-footer">
+    <div class="card-footer" v-if="authorize('owns', thread)">
         <button class="btn btn-secondary btn-sm" @click="editing = true">Edit</button>
     </div>
 </div>
